679f17ef91cce7b7deddc1117dd3c4fdc6f9f44f
Don Denton <don@happycollision.com>
2015-04-14 11:15:53 -0500

    Adjust JSON to reflect Andrey's changes

--- files/help-json.js
+++ files/help-json.js
@@ -1,43 +1,5 @@
 [
   {
-    "name":"Basic Templates",
-    "type":"section"
-  },
-  {
-    "name":"Working with images",
-    "type":"video",
-    "data":"/Video/help/basic/working-with-images.mp4"
-  },
-  {
-    "name":"Adding audio",
-    "type":"video",
-    "data":"/Video/help/basic/adding-audio.mp4"
-  },
-  {
-    "name":"Customizing text",
-    "type":"video",
-    "data":"/Video/help/basic/customizing-text.mp4"
-  },
-  {
-    "name":"Preview and finalize",
-    "type":"video",
-    "data":"/Video/help/basic/preview-and-finalize.mp4"
-  },
-  {
-    "name":"Pro Templates",
-    "type":"section"
-  },
-  {
-    "name":"Pro templates overview",
-    "type":"html",
-    "data":"/DonContentPasteboard/pro-overview.html"
-  },
-  {
-    "name":"Adding Audio",
-    "type":"video",
-    "data":"/Video/help/pro/adding-audio.mp4"
-  },
-  {
     "name":"Walkthroughs",
     "type":"section"
   },
@@ -50,5 +12,43 @@
     "name":"Basic templates walkthrough",
     "type":"youTube",
     "data":"BmQmYXOBxyk"
+  },
+  {
+    "name":"Basic Templates",
+    "type":"section"
+  },
+  {
+    "name":"Working with images",
+    "type":"video",
+    "data":"http://mediarobotvideo.s3.amazonaws.com/help/basic/working-with-images.mp4"
+  },
+  {
+    "name":"Adding audio",
+    "type":"video",
+    "data":"http://mediarobotvideo.s3.amazonaws.com/help/basic/adding-audio.mp4"
+  },
+  {
+    "name":"Customizing text",
+    "type":"video",
+    "data":"http://mediarobotvideo.s3.amazonaws.com/help/basic/customizing-text.mp4"
+  },
+  {
+    "name":"Preview and finalize",
+    "type":"video",
+    "data":"http://mediarobotvideo.s3.amazonaws.com/help/basic/preview-and-finalize.mp4"
+  },
+  {
+    "name":"Pro Templates",
+    "type":"section"
+  },
+  {
+    "name":"Pro templates overview",
+    "type":"html",
+    "data":"/HelpContent/pro-overview.html"
+  },
+  {
+    "name":"Adding Audio",
+    "type":"video",
+    "data":"http://mediarobotvideo.s3.amazonaws.com/help/pro/adding-audio.mp4"
   }
 ]
